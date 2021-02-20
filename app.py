17a972490850c4237245f016a482007797d248d3
farrej10 <farrej10@tcd.ie>
2021-02-20 13:52:23 +0000

    filtering by start end date on all users

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
 mysql = MySQL(app)
 
 
-def get_weigths():  # Return weights table
+def get_weigths(start, end):  # Return weights table
 
     cur = mysql.connection.cursor()
     try:
         cur.execute(
-            "SELECT user_id,CAST(timestamp AS CHAR(30)),weight FROM weightlossgrapher.weights")
+            "SELECT user_id,CAST(timestamp AS CHAR(30)),weight FROM weightlossgrapher.weights WHERE timestamp BETWEEN  %s AND %s ORDER BY timestamp;", (start, end))
     except Exception as e:
         # If this fails its likely an error related to connection to mysql or lack there of
         return jsonify(error=str(e)), status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -47,7 +47,7 @@ def get_weights_by_user(user, start, end):  # Return all weights for a user
     cur = mysql.connection.cursor()
     try:
         cur.execute(
-            "SELECT user_id,CAST(timestamp AS CHAR(30)),weight FROM weightlossgrapher.weights WHERE user_id = %s AND timestamp BETWEEN  %s AND %s;", (user, start, end))
+            "SELECT user_id,CAST(timestamp AS CHAR(30)),weight FROM weightlossgrapher.weights WHERE user_id = %s AND timestamp BETWEEN  %s AND %s ORDER BY timestamp;", (user, start, end))
     except Exception as e:
         # Using 400 as its likely a bad request
         return jsonify(error=str(e)), status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -68,7 +68,7 @@ def get_weights_by_name(name, start, end):  # return all weights for a user's na
     cur = mysql.connection.cursor()
     try:
         cur.execute(
-            "SELECT user_id, name, timestamp, weight FROM user t1 INNER JOIN weights t2 ON t1.id = t2.user_id WHERE name = %s AND timestamp BETWEEN  %s AND %s;", (name, start, end))
+            "SELECT user_id, name, timestamp, weight FROM user t1 INNER JOIN weights t2 ON t1.id = t2.user_id WHERE name = %s AND timestamp BETWEEN  %s AND %s ORDER BY timestamp;", (name, start, end))
     except Exception as e:
         # Using 400 as its likely a bad request
         return jsonify(error=str(e)), status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -196,19 +196,23 @@ def weights():
         name = request.args.get('name', default=None, type=str)
         start = request.args.get(
             'start', default='1970-01-01 12:00:00', type=str)
-        end = request.args.get('end', default=datetime.datetime.fromtimestamp(
-            time.time()).strftime('%Y-%m-%d %H:%M:%S'), type=str)
+        now = datetime.datetime.fromtimestamp(
+            time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        end = request.args.get('end', default=now, type=str)
+
+        if(end !=now):
+            end += " 23:59:59"
 
         if(start == end):
-            start = start+" 00:00:00"
-            end = end + " 23:59:59"
+            start +=" 00:00:00"
+            end += " 23:59:59"
         # user is unique therfore select based on that
         if (user != None):
             return get_weights_by_user(user, start, end)
         if (name != None):
             return get_weights_by_name(name, start, end)
         else:
-            return get_weigths()
+            return get_weigths(start, end)
 
     if request.method == "POST":
         # should be no params all info should be in the body
