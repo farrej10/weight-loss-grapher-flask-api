78994b0862122b5f88dcd9a48e84743ef23128dd
farrej10 <farrej10@tcd.ie>
2021-02-20 01:45:01 +0000

    added catches for unknown params, make get and create methods for users

--- app.py
+++ app.py
@@ -39,15 +39,15 @@ def get_weigths():  # Return weights table
     if(results):
         return jsonify(results), status.HTTP_200_OK
     else:
-        return jsonify(error="Internal Server Error"), status.HTTP_500_INTERNAL_SERVER_ERROR
+        return jsonify(error="Not Found"), status.HTTP_404_NOT_FOUND
 
 
-def get_weights_by_user(user,start,end):  # Return all weights for a user
+def get_weights_by_user(user, start, end):  # Return all weights for a user
 
     cur = mysql.connection.cursor()
     try:
         cur.execute(
-            "SELECT user_id,CAST(timestamp AS CHAR(30)),weight FROM weightlossgrapher.weights WHERE user_id = %s AND timestamp BETWEEN  %s AND %s;", (user,start,end))
+            "SELECT user_id,CAST(timestamp AS CHAR(30)),weight FROM weightlossgrapher.weights WHERE user_id = %s AND timestamp BETWEEN  %s AND %s;", (user, start, end))
     except Exception as e:
         # Using 400 as its likely a bad request
         return jsonify(error=str(e)), status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -60,15 +60,15 @@ def get_weights_by_user(user,start,end):  # Return all weights for a user
     if(results):
         return jsonify(results), status.HTTP_200_OK
     else:
-        return jsonify(error="User Not Found or No Weight Data with that User"), status.HTTP_404_NOT_FOUND
+        return jsonify(error="Not Found"), status.HTTP_404_NOT_FOUND
 
 
-def get_weights_by_name(name,start,end):  # return all weights for a user's name
+def get_weights_by_name(name, start, end):  # return all weights for a user's name
 
     cur = mysql.connection.cursor()
     try:
         cur.execute(
-            "SELECT user_id, name, timestamp, weight FROM user t1 INNER JOIN weights t2 ON t1.id = t2.user_id WHERE name = %s AND timestamp BETWEEN  %s AND %s;", (name,start,end))
+            "SELECT user_id, name, timestamp, weight FROM user t1 INNER JOIN weights t2 ON t1.id = t2.user_id WHERE name = %s AND timestamp BETWEEN  %s AND %s;", (name, start, end))
     except Exception as e:
         # Using 400 as its likely a bad request
         return jsonify(error=str(e)), status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -81,7 +81,7 @@ def get_weights_by_name(name,start,end):  # return all weights for a user's name
     if(results):
         return jsonify(results), status.HTTP_200_OK
     else:
-        return jsonify(error="Name Not Found or No Weight Data with that Name"), status.HTTP_404_NOT_FOUND
+        return jsonify(error="Not Found"), status.HTTP_404_NOT_FOUND
 
 
 def create_weight_for_user(request):  # Create a weight entry for a user
@@ -113,6 +113,65 @@ def create_weight_for_user(request):  # Create a weight entry for a user
     return jsonify(content), status.HTTP_201_CREATED
 
 
+def get_users(id, name):  # Return weights table
+
+    cur = mysql.connection.cursor()
+    mysqlcommand = ""
+    searchparam = ""
+    if(id != None):
+        mysqlcommand = "SELECT * FROM weightlossgrapher.user WHERE id = %s;"
+        searchparam = id
+    elif(name != None):
+        mysqlcommand = "SELECT * FROM weightlossgrapher.user WHERE name = %s;"
+        searchparam = name
+    else:
+        mysqlcommand = "SELECT * FROM weightlossgrapher.user;"
+    try:
+        if(searchparam == ""):
+            cur.execute(mysqlcommand)
+        else:
+            cur.execute(mysqlcommand, (searchparam,))
+    except Exception as e:
+        # If this fails its likely an error related to connection to mysql or lack there of
+        return jsonify(error=str(e)), status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    fields = [i[0] for i in cur.description]
+    results = [dict(zip(fields, row)) for row in cur.fetchall()]
+    cur.close()
+
+    if(results):
+        return jsonify(results), status.HTTP_200_OK
+    else:
+        return jsonify(error="Not Found"), status.HTTP_404_NOT_FOUND
+
+
+def create_user():
+    # Check if request has json and has the two required fields
+    if not request.json or not 'name' in request.json:
+        return jsonify(error="400 Bad Request"), status.HTTP_400_BAD_REQUEST
+
+    # extract content + make timestamp
+    content = request.json
+    name = content['name']
+
+    cur = mysql.connection.cursor()
+    try:
+        cur.execute(
+            "INSERT INTO `weightlossgrapher`.`user` (`name`) VALUES (%s);", (name,))
+        cur.execute(
+            "SELECT * FROM `weightlossgrapher`.`user` WHERE `id`= LAST_INSERT_ID()")
+        mysql.connection.commit()
+    except Exception as e:
+        mysql.connection.rollback()
+        return jsonify(error=str(e)), status.HTTP_400_BAD_REQUEST
+
+    fields = [i[0] for i in cur.description]
+    results = [dict(zip(fields, row)) for row in cur.fetchall()]
+
+    cur.close()
+    return jsonify(results), status.HTTP_201_CREATED
+
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
@@ -120,27 +179,68 @@ def index():
 
 @app.route('/weights', methods=['GET', 'POST'])
 def weights():
-    if request.method == "GET":
 
+    if request.method == "GET":
+        paramslist = list(request.args.to_dict().keys())
+        if 'user' in paramslist:
+            paramslist.remove('user')
+        if 'name' in paramslist:
+            paramslist.remove('name')
+        if 'start' in paramslist:
+            paramslist.remove('start')
+        if 'end' in paramslist:
+            paramslist.remove('end')
+        if(paramslist):
+            return jsonify(error="400 Bad Request Unkown Parameters: \'{}\'".format('\', \''.join(paramslist))), status.HTTP_400_BAD_REQUEST
         user = request.args.get('user', default=None, type=str)
         name = request.args.get('name', default=None, type=str)
-        start = request.args.get('start',default='1970-01-01 12:00:00', type=str)
-        end = request.args.get('end',default=datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'),type=str)
-        
-        
-        if(start==end):
-            start=start+" 00:00:00"
-            end=end+ " 23:59:59"
+        start = request.args.get(
+            'start', default='1970-01-01 12:00:00', type=str)
+        end = request.args.get('end', default=datetime.datetime.fromtimestamp(
+            time.time()).strftime('%Y-%m-%d %H:%M:%S'), type=str)
+
+        if(start == end):
+            start = start+" 00:00:00"
+            end = end + " 23:59:59"
         # user is unique therfore select based on that
         if (user != None):
-            return get_weights_by_user(user,start,end)
+            return get_weights_by_user(user, start, end)
         if (name != None):
-            return get_weights_by_name(name,start,end)
+            return get_weights_by_name(name, start, end)
         else:
             return get_weigths()
 
     if request.method == "POST":
+        # should be no params all info should be in the body
+        paramslist = list(request.args.to_dict().keys())
+        if(paramslist):
+            return jsonify(error="400 Bad Request Unkown Parameters: \'{}\'".format('\', \''.join(paramslist))), status.HTTP_400_BAD_REQUEST
         return create_weight_for_user(request)
+
+
+@app.route('/user', methods=['GET', 'POST'])
+def user():
+
+    if request.method == "GET":
+        paramslist = list(request.args.to_dict().keys())
+        if 'user' in paramslist:
+            paramslist.remove('user')
+        if 'name' in paramslist:
+            paramslist.remove('name')
+        if(paramslist):
+            return jsonify(error="400 Bad Request Unkown Parameters: \'{}\'".format('\', \''.join(paramslist))), status.HTTP_400_BAD_REQUEST
+        else:
+            user = request.args.get('user', default=None, type=str)
+            name = request.args.get('name', default=None, type=str)
+            return get_users(user, name)
+
+    if request.method == "POST":
+        # should be no params all info should be in the body
+        paramslist = list(request.args.to_dict().keys())
+        if(paramslist):
+            return jsonify(error="400 Bad Request Unkown Parameters: \'{}\'".format('\', \''.join(paramslist))), status.HTTP_400_BAD_REQUEST
+        else:
+            return create_user()
 
 
 if __name__ == '__main__':
